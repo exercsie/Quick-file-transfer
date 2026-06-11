3456e9b4d7b4673ec5f8669c9aceef4122f8f060
exercsie <hammondtristan62@gmail.com>
2026-06-11 14:56:15 +1000

    Added some starter server connection functions

--- src/Server/StartUpServer.cpp
+++ src/Server/StartUpServer.cpp
@@ -1,3 +1,42 @@
 #include "StartUpServer.h"
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <iostream>
+
+void Server::createServerFileDescriptor() {
+    serverFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if(serverFileDescriptor == -1) {
+        std::cout << "Socket failed!\n";
+        exit(1);
+    }
+
+    std::cout << "Socket created!\n";
+}
+
+int Server::getServerFileDescriptorSocket() const {
+    return serverFileDescriptor;
+}
+
+void Server::bindSocket(const int& PORT) {
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+    int bindSock = bind(serverFileDescriptor, (sockaddr*)(&serverAddress), sizeof(serverAddress));
+    if(bindSock == -1) {
+        std::cout << "Bind failed!\n";
+    }
+
+    std::cout << "Socket binded!\n";
+}
+
+void Server::listenOnServerFileDescriptor(const int& PORT) {
+    int listening = listen(serverFileDescriptor, 1);
+    if(listening == -1) {
+        std::cout << "Listening failed!\n";
+    }
+
+    std::cout << "Listening on PORT: " << PORT << "...";
+}
+
--- src/Server/StartUpServer.h
+++ src/Server/StartUpServer.h
@@ -1,5 +1,12 @@
 #pragma once
 
 class Server {
-
+public:
+    void createServerFileDescriptor();
+    int getServerFileDescriptorSocket() const;
+    void bindSocket(const int& PORT);
+    void listenOnServerFileDescriptor(const int& PORT);
+    void acceptConnection();
+private:
+    int serverFileDescriptor;
 };
